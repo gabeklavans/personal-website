bd3edf5e8983b3a9eca1dac63a63b159b0f08b21
Gabriel Klavans <gabeklav@bu.edu>
2019-12-22 03:42:11 -0500

    Improved responsive navbar

--- navbar.js
+++ navbar.js
@@ -1,11 +1,15 @@
 /* jshint esversion: 6 */
+
+// close menu initially
+$(".nav-item").toggle();
+
 let button = document.querySelector('#nav-button');
 button.onclick = () => {
     // toggle between open and closed hamburger
-    if (button.textContent == "☰") {
-        button.textContent = '✖';
-    } else {
+    if (button.textContent == '✖') {
         button.textContent = '☰';
+    } else {
+        button.textContent = '✖';
     }
 
     // toggle between open and closed nav menu
